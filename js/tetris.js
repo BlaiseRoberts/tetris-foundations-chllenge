8129bc5dc6bc21fe79e0c52c17981460cc7b8671
Blaise Roberts <blaiseroberts@Blaises-MacBook-Pro-2.local>
2017-01-21 23:23:41 -0600

    added extra blocks

--- js/tetris.js
+++ js/tetris.js
@@ -112,6 +112,121 @@ ctx.stroke()
 
 
 
+//draw line to divide staging area
+ctx.beginPath();
+ctx.moveTo(200,0);
+ctx.lineTo(200,480);
+ctx.stroke();
+
+
+
+
+//first block
+ctx.beginPath()
+ctx.rect(240,20,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(240,40,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(220,40,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(260,40,20,20)
+ctx.stroke()
+
+//second block
+ctx.beginPath()
+ctx.rect(220,100,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(240,100,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(220,80,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(240,80,20,20)
+ctx.stroke()
+
+//third block
+ctx.beginPath()
+ctx.rect(240,160,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(240,180,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(240,200,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(240,140,20,20)
+ctx.stroke()
+
+//fourth block
+ctx.beginPath()
+ctx.rect(260,260,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(260,240,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(220,240,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(240,240,20,20)
+ctx.stroke()
+
+//fifth block
+ctx.beginPath()
+ctx.rect(260,300,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(220,300,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(220,320,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(240,300,20,20)
+ctx.stroke()
+
+//sixth block
+ctx.beginPath()
+ctx.rect(240,360,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(260,360,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(240,380,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(220,380,20,20)
+ctx.stroke()
+
+//seventh block
+ctx.beginPath()
+ctx.rect(240,420,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(220,420,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(240,440,20,20)
+ctx.stroke()
+ctx.beginPath()
+ctx.rect(260,440,20,20)
+ctx.stroke()
+
+
+
+
+
+
+
+
+
 
 
 
